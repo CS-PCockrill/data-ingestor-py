1c881ca953f96fc1800a277dc26f069cfa406367
Patrick <queuedelivery@gmail.com>
2025-01-17 16:43:09 -0500

    Implementing abstract base classes, factory pattern for processing and logging

--- db/postgres_query_builder.py
+++ db/postgres_query_builder.py
@@ -2,9 +2,12 @@ from db.query_builder import QueryBuilder
 
 class PostgresQueryBuilder(QueryBuilder):
     def build_insert_query(self, columns):
-        column_list = ", ".join(columns)
-        placeholders = ", ".join(["%s"] * len(columns))  # Postgres uses `%s` for placeholders
-        return f"INSERT INTO {self.table_name} ({column_list}) VALUES ({placeholders}) RETURNING id"
+        # Generate column list and placeholder
+        col_list = ", ".join(columns)
+        return f"INSERT INTO {self.table_name} ({col_list}) VALUES %s RETURNING id;"
+        # column_list = ", ".join(columns)
+        # placeholders = ", ".join(["%s"] * len(columns))  # Postgres uses `%s` for placeholders
+        # return f"INSERT INTO {self.table_name} ({column_list}) VALUES ({placeholders}) RETURNING id"
 
     def build_update_query(self, columns, condition="id = %s"):
         set_clause = ", ".join([f"{col} = %s" for col in columns])
